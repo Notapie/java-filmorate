617eeac4d5c4b09338d07da4407ae1d1c819ee5b
notapie <notapie@yandex.ru>
2023-05-16 20:38:57 +0300

    refactor: remove unused import

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -6,7 +6,6 @@ import lombok.Value;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.time.Duration;
 import java.time.LocalDate;
 
 @Value
