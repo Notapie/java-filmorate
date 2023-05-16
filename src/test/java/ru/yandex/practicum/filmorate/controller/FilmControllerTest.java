1fd9a8f53d32fb9bbbdf5217bb28c657707868b3
notapie <notapie@yandex.ru>
2023-05-16 22:56:40 +0300

    feat: add film controller tests

---
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -0,0 +1,177 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilmControllerTest {
+    private FilmController controller;
+
+    @BeforeEach
+    public void updateController() {
+        controller = new FilmController();
+    }
+
+    @Test
+    public void shouldAddNewFilm() {
+        final Film film = controller.create(
+                Film.builder()
+                        .name("film")
+                        .description("film for tests")
+                        .releaseDate(LocalDate.of(1999, 7, 2))
+                        .duration(190)
+                        .build()
+        );
+
+        final Film expectedFilm = Film.builder()
+                .id(1)
+                .name("film")
+                .description("film for tests")
+                .releaseDate(LocalDate.of(1999, 7, 2))
+                .duration(190)
+                .build();
+
+        assertEquals(expectedFilm, film);
+        assertEquals(expectedFilm, controller.get().iterator().next());
+    }
+
+    @Test
+    public void shouldUpdateFilm() {
+        controller.create(
+                Film.builder()
+                        .name("film")
+                        .description("film for tests")
+                        .releaseDate(LocalDate.of(1999, 7, 2))
+                        .duration(190)
+                        .build()
+        );
+        final Film film = controller.update(
+                Film.builder()
+                        .id(1)
+                        .name("updated")
+                        .description("updated film for tests")
+                        .releaseDate(LocalDate.of(1999, 7, 2))
+                        .duration(190)
+                        .build()
+        );
+        final Film expectedFilm = Film.builder()
+                .id(1)
+                .name("updated")
+                .description("updated film for tests")
+                .releaseDate(LocalDate.of(1999, 7, 2))
+                .duration(190)
+                .build();
+
+        assertEquals(expectedFilm, film);
+        assertEquals(1, controller.get().size());
+        assertEquals(expectedFilm, controller.get().iterator().next());
+    }
+
+    @Test
+    public void shouldReturnAllFilms() {
+        controller.create(
+                Film.builder()
+                        .name("film")
+                        .description("film for tests")
+                        .releaseDate(LocalDate.of(1999, 7, 2))
+                        .duration(190)
+                        .build()
+        );
+        controller.create(
+                Film.builder()
+                        .name("film2")
+                        .description("film2 for tests")
+                        .releaseDate(LocalDate.of(1999, 7, 2))
+                        .duration(200)
+                        .build()
+        );
+
+        Film[] expected = {
+                Film.builder()
+                        .id(1)
+                        .name("film")
+                        .description("film for tests")
+                        .releaseDate(LocalDate.of(1999, 7, 2))
+                        .duration(190)
+                        .build(),
+                Film.builder()
+                        .id(2)
+                        .name("film2")
+                        .description("film2 for tests")
+                        .releaseDate(LocalDate.of(1999, 7, 2))
+                        .duration(200)
+                        .build(),
+
+        };
+
+        assertArrayEquals(expected, controller.get().toArray());
+    }
+
+    @Test
+    public void shouldExceptionIfNewFilmNameIsNullOrBlank() {
+        ValidationException e = assertThrows(ValidationException.class, () -> {
+            controller.create(
+                    Film.builder()
+                            .description("desc")
+                            .releaseDate(LocalDate.of(1999, 7, 2))
+                            .duration(21)
+                            .build()
+            );
+        });
+
+        assertEquals("film name must be not null or blank.", e.getMessage());
+    }
+
+    @Test
+    public void shouldExceptionIfNewFilmDescSizeMoreThan200() {
+        ValidationException e = assertThrows(ValidationException.class, () -> {
+            controller.create(
+                    Film.builder()
+                            .name("film")
+                            .description("desc".repeat(51))
+                            .releaseDate(LocalDate.of(1999, 7, 2))
+                            .duration(21)
+                            .build()
+            );
+        });
+
+        assertEquals("film desc must be less than 200.", e.getMessage());
+    }
+
+    @Test
+    public void shouldExceptionIfNewFilmReleaseDateIsBefore1895() {
+        ValidationException e = assertThrows(ValidationException.class, () -> {
+            controller.create(
+                    Film.builder()
+                            .name("film")
+                            .description("desc")
+                            .releaseDate(LocalDate.of(1800, 7, 2))
+                            .duration(21)
+                            .build()
+            );
+        });
+
+        assertEquals("Invalid film release date.", e.getMessage());
+    }
+
+    @Test
+    public void shouldExceptionIfNewFilmDurationIsNegative() {
+        ValidationException e = assertThrows(ValidationException.class, () -> {
+            controller.create(
+                    Film.builder()
+                            .name("film")
+                            .description("desc")
+                            .releaseDate(LocalDate.of(1999, 7, 2))
+                            .duration(-23)
+                            .build()
+            );
+        });
+
+        assertEquals("Invalid film duration. Duration must be positive.", e.getMessage());
+    }
+}
