be18be291f33c4f633473b2bac6e4677d4e76c01
notapie <notapie@yandex.ru>
2023-05-23 00:37:02 +0300

    feat: add UserStorage interface structure

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -1,4 +1,25 @@
 package ru.yandex.practicum.filmorate.storage;
 
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
+
 public interface UserStorage {
+
+    User createUser(final User user);
+
+    User updateUser(final User user);
+
+    User removeUser(final int id);
+
+    User removeUser(final String email);
+
+    User getUserByEmail(final String email);
+
+    User getUserById(final int id);
+
+    Collection<User> getUserFriends(final int userId);
+
+    Collection<User> getMutualFriends(final int firstUserId, final int secondUserId);
+
 }
