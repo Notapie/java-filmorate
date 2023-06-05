c42a2870b467edf099256c59a9e033f2265ed758
notapie <notapie@yandex.ru>
2023-06-05 03:34:02 +0300

    feat: add rest controller advice

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorController.java
@@ -0,0 +1,39 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.exception.AlreadyExistsException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.ErrorResponse;
+
+@RestControllerAdvice
+public class ErrorController {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse notFound(final NotFoundException e) {
+        return new ErrorResponse(
+          "Not found", e.getMessage()
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse alreadyExists(final AlreadyExistsException e) {
+        return new ErrorResponse(
+                "Already exists", e.getMessage()
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse validationError(final ValidationException e) {
+        return new ErrorResponse(
+                "Validation error", e.getMessage()
+        );
+    }
+
+}
