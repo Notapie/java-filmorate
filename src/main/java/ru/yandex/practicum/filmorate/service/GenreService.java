2dee71e036f0b7c74bece37a01315ae276f7e2c6
notapie <notapie@yandex.ru>
2023-08-14 10:21:14 +0900

    comment: add validation in genre service

--- src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.storage.GenreStorage;
@@ -15,8 +16,8 @@ import java.util.Collection;
 public class GenreService {
     private final GenreStorage storage;
 
-    // TODO: add validation
     public Genre createNewGenre(final Genre newObject) {
+        validate(newObject);
         final Genre result = storage.create(newObject);
         log.debug("Added new genre: " + result);
         return result;
@@ -26,6 +27,7 @@ public class GenreService {
         if (newObject.getId() == null) {
             throw new ValidationException("Genre id must be not null");
         }
+        validate(newObject);
 
         final Genre result = storage.update(newObject);
         log.debug("Updated genre: " + result);
@@ -40,5 +42,16 @@ public class GenreService {
     public Genre getGenreById(final int id) {
         log.debug("Getting genre with id: " + id);
         return storage.getById(id);
+    }
+
+    private void validate(final Genre genre) {
+        final int maxNameLength = 32;
+
+        if (!StringUtils.hasText(genre.getName())) {
+            throw  new ValidationException("Genre name cannot be blank or null");
+        }
+        if (genre.getName().length() > maxNameLength) {
+            throw new ValidationException("The length of the genre name exceeds " + maxNameLength + " characters");
+        }
     }
 }
