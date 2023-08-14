09640feeaa2bd2daccb011f54b7b5abc8df4deac
notapie <notapie@yandex.ru>
2023-08-14 10:04:44 +0900

    comment: add logs in GenreService

--- src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Genre;
@@ -8,28 +9,36 @@ import ru.yandex.practicum.filmorate.storage.GenreStorage;
 
 import java.util.Collection;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class GenreService {
     private final GenreStorage storage;
 
-    // TODO: add logs and validation
+    // TODO: add validation
     public Genre createNewGenre(final Genre newObject) {
-        return storage.create(newObject);
+        final Genre result = storage.create(newObject);
+        log.debug("Added new genre: " + result);
+        return result;
     }
 
     public Genre updateGenre(final Genre newObject) {
         if (newObject.getId() == null) {
             throw new ValidationException("Genre id must be not null");
         }
-        return storage.update(newObject);
+
+        final Genre result = storage.update(newObject);
+        log.debug("Updated genre: " + result);
+        return result;
     }
 
     public Collection<Genre> getAllGenres() {
+        log.debug("Getting all genres");
         return storage.getAll();
     }
 
     public Genre getGenreById(final int id) {
+        log.debug("Getting genre with id: " + id);
         return storage.getById(id);
     }
 }
